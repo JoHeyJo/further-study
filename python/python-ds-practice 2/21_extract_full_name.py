597a32d8239c3e2d20df013be0bcd2b095662c09
jf <jpf0628@gmail.com>
2023-02-07 22:28:30 -0800

    solved 21

--- python/python-ds-practice 2/21_extract_full_name.py
+++ python/python-ds-practice 2/21_extract_full_name.py
@@ -14,3 +14,4 @@ def extract_full_names(people):
         >>> extract_full_names(names)
         ['Ada Lovelace', 'Grace Hopper']
     """
+    return [f"{name.get('first')} {name.get('last')}" for name in people]
