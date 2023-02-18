c65c5fa8b683a994a2355d4a0f49a18038a90ece
jf <jpf0628@gmail.com>
2023-02-17 16:26:01 -0800

    created defauly prop for number of boxes and a function to create an array with that number of random colors on mount

--- react/react-state-eightball-colors/color-boxess-ts/src/Container.tsx
+++ react/react-state-eightball-colors/color-boxess-ts/src/Container.tsx
@@ -14,29 +14,39 @@ const defaultColors = [
  * 
  * Props:
  * - colors
+ * - numBoxes: default number of boxes
  * 
  * State:
  * - boxes: []
  */
-function Container(){
-  const [colors, setColors] = useState<string[]>(defaultColors)
+function Container({ numBoxes = 16 }) {
+  const [colors, setColors] = useState<string[]>(createInitialBoxes)
+
+  /** Return array of random colors */
+  function createInitialBoxes() {
+    return Array.from(
+      { length: numBoxes },
+      () => chooseRandom(defaultColors)
+
+    )
+  }
 
   /** on button click: changes color of selected box */
-  function changeColor():void{
+  function changeColor(): void {
     const newColor = chooseRandom(colors);
     const idx = randomNum(colors.length);
     setColors(c => {
-      let copy = [...c]
+      let copy = [...c] // don't forget to create copy of state before mutating
       copy[idx] = newColor
       return copy
     })
   }
 
-  const boxes = colors.map((color,i) => <Box key={i} color={color} />)
+  const boxes = colors.map((color, i) => <Box key={i} color={color} />)
 
   return (
     <div className='Container-box'
-    onClick={changeColor}>
+      onClick={changeColor}>
       {boxes}
     </div>
   )
